e64dabaf39db5d0badc12e89539e8d47b7a5e83d
vladdd61 <30327491+vladdd61@users.noreply.github.com>
2019-10-03 18:12:14 +0300

    Update apply.js

--- apply.js
+++ apply.js
@@ -2,6 +2,9 @@ const apply = (q, f, arg) => {
    if (q === 0) {
      return arg;
    } 
+   if (q < 0) {
+     return "Not negative";
+   }
    return apply (q-1, f, f(arg)); 
 }
 export default apply;
