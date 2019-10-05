a1279c13eda33ac58e8fbc81c77a1da352252cec
vladdd61 <30327491+vladdd61@users.noreply.github.com>
2019-10-05 18:38:16 +0300

    Update substr.js

--- substr.js
+++ substr.js
@@ -2,21 +2,21 @@ const substr = (str, startingIndex = 0, lengthString = str.length) => {
     if (startingIndex < 0){
        startingIndex = 0;
     }
-     if (startingIndex > str.length){
+    if (startingIndex > str.length){
        return " " ;
     }
-   if (lengthString < 0){
+    if (lengthString < 0){
        lengthString = 1;
     } 
-   if (lengthString > str.length) {
+    if (lengthString > str.length) {
        lengthString = str.length;
     }
-   if ((startingIndex + lengthString) > str.length){
-      lengthString -= startingIndex;
+    if ((startingIndex + lengthString) > str.length){
+       lengthString -= startingIndex;
     }
- let result = ""; 
-  for (let i = startingIndex; i < startingIndex + lengthString ; i++){
-  result += str[i];
+    let result = ""; 
+    for (let i = startingIndex; i < startingIndex + lengthString ; i++){
+       result += str[i];
     }
  return result;
 } 
